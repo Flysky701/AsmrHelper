0067240b90055a4131b1e9a6e76f59b886b0d16a
Flysky701 <18458871liu@gmail.com>
2026-04-10 17:14:30 +0800

    fix: 删除脚本文件，删除部分格式

--- fix_preview_call.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/gui/views/single_tab.py', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('self.single_preview_btn.clicked.connect(self.main_window.preview_voice)', 'self.single_preview_btn.clicked.connect(self.preview_voice)')
-text = text.replace('self.main_window.preview_thread = PreviewWorkerThread', 'from src.gui.workers.pipeline_worker import PreviewWorkerThread\n        self.main_window.preview_thread = PreviewWorkerThread')
-
-with open('src/gui/views/single_tab.py', 'w', encoding='utf-8') as f:
-    f.write(text)
